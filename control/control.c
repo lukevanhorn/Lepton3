890a24d053270bc82ccb44cdd890af1eecafdae5
Luke Van Horn <lukevanhorn@gmail.com>
2017-09-02 03:51:58 +0200

    control

--- control/control.c
+++ control/control.c
@@ -35,6 +35,17 @@
 
 uint8_t lep_status_reg[2] = { 0x00, 0x02 };
 uint8_t lep_command_reg[2] = { 0x00, 0x04 };
+uint8_t lep_data_reg[2] = { 0x00, 0x08 };
+
+struct write_register {
+    uint16_t address;
+    uint16_t data; 
+};
+
+struct read_register {
+    uint16_t address;
+};
+
 
 
 int device;
@@ -44,14 +55,25 @@ uint8_t booted, boot_mode, busy;
 uint8_t status[2] = {0};
 //uint8_t command[4] = { 0x00, 0x04, 0x00, 0x00 };
 //uint8_t length_reg[4] = { 0x00, 0x06 };
-uint8_t rx_data[1024] = {0};
-uint8_t tx_data[1024] = {0};
+uint16_t rx_data[16] = {0};
+uint16_t tx_data[16] = {0};
 
 /* utility function for rpi to make big endian */
-uint16_t changeEndian(uint16_t val) {
+uint16_t regRE(uint16_t val) {
     uint16_t res = 0;
     res |= (val & 0x00FF) << 8;
     res |= (val & 0xFF00) >> 8;
+    
+    return res;
+}
+
+/* convert enums endianness */
+int enumRE(int val) {
+    int res = 0;
+    res |= (val & 0x0000FF00) << 8;
+    res |= (val & 0x000000FF) << 24;
+    res |= (val & 0xFF000000) >> 24;
+    res |= (val & 0x00FF0000) >> 8;
     
     return res;
 }
@@ -106,39 +128,56 @@ int waitForReady(int tries) {
 int readData(uint8_t mod, uint8_t com, uint16_t length) {
 
     int count = 0;
+    struct write_register tx;    
     uint8_t command[4] = { 0x00, 0x00, 0x00, 0x00 };
 
-
     if(waitForReady(10)) {
         return -1;
     }
     
-    /* set the data length register (big endian) */
-    command[1] = 0x06; /* data length register */
-    command[2] = (uint8_t)(length & 0x00FF);
-    command[3] = (uint8_t)((length & 0xFF00) >> 8);
-    write(device, command, 4);    
     
-    /* set module id, command id, and command type */
-    command[1] = 0x04;  /* command register */
-    command[2] = 0x0F & mod;
-    command[3] = com + LEP_COM_GET;
-    
+    tx.address = regRE(6);
+    tx.data = regRE(length);
+    write(device, &tx, sizeof(write_register));     
+
     /* set the oem bit if necessary */
     if(mod == LEP_MOD_OEM || mod == LEP_MOD_RAD) {
-        command[2] |= 0x40;
+        mod |= 0x40;
     }
+    
+    tx.address = regRE(4);
+    tx.data = ((0x00FF & mod) << 8) + com;
+    write(device, &tx, sizeof(write_register));   
+
+    /* set the data length register (big endian) */
+    //command[1] = 0x06; /* data length register */
+    //command[2] = (uint8_t)(length & 0x00FF);
+    //command[3] = (uint8_t)((length & 0xFF00) >> 8);
+    //write(device, command, 4);    
+    
+    /* set module id, command id, and command type */
+    //command[1] = 0x04;  /* command register */
+    //command[2] = 0x0F & mod;
+    //command[3] = com + LEP_COM_GET;
+    
+    /* set the oem bit if necessary */
+    //if(mod == LEP_MOD_OEM || mod == LEP_MOD_RAD) {
+    //    command[2] |= 0x40;
+    //}
 
     /* write to command register */
-    write(device, command, 4);
+    //write(device, command, 4);
     
     if(waitForReady(10)) {
         return -1;
     }
+
+    tx.address = regRE(8);
+    write(device, &tx, 2);
     
     /* send the read command for the data register */
-    command[1] = 0x08;  /* data register */
-    write(device, command, 2);    
+    //command[1] = 0x08;  /* data register */
+    //write(device, command, 2);    
     
     /* read data bytes = 2 x word length*/
     count = read(device, rx_data, (length * 2));
@@ -159,10 +198,13 @@ int readData(uint8_t mod, uint8_t com, uint16_t length) {
    com: [Command ID] (0x00, 0x08, 0x0C, etc...)
    len: [words to read] (16bit words)
 */
-int writeData(uint8_t mod, uint8_t com, uint8_t * data, uint8_t length) {
+int writeData(uint8_t mod, uint8_t com, uint16_t * data, uint16_t length) {
 
-    uint8_t i;
+    uint8_t i = 0;
     int count = 0;
+
+    struct write_register tx;    
+
     uint8_t command[20] = {0};
 
     if(waitForReady(10)) {
@@ -170,38 +212,46 @@ int writeData(uint8_t mod, uint8_t com, uint8_t * data, uint8_t length) {
     }
 
     /* set the data registers */
-    /*
-    for(i = 0; i < length; i += 2) {
-        command[1] = 0x08 + i; 
-        command[2] = (uint8_t)data[i];
-        command[3] = (uint8_t)data[i+1];
-        write(device, command, 4);    
-    }
-    */
-    
-    command[1] = 0x08;
-    memcpy(&command[2], data, (length * 2));
-    
-    write(device, command, (length * 2) + 2); 
-    
-    /* set the data length register (big endian) */
-    command[1] = 0x06; /* data length register */
-    command[2] = 0x00;
-    command[3] = length;
-    write(device, command, 4);    
-    
-    /* set module id, command id, and command type */
-    command[1] = 0x04;  /* command register */
-    command[2] = 0x0F & mod;
-    command[3] = com + 0x01;
-    
+
+
+    while(i < length) {
+        tx.address = regRE(8 + (i *2));
+        tx.data = data[i++];
+
+        write(device, &tx, sizeof(write_register));         
+    } 
+
+    tx.address = regRE(6);
+    tx.data = regRE(length);
+    write(device, &tx, sizeof(write_register));     
+
     /* set the oem bit if necessary */
     if(mod == LEP_MOD_OEM || mod == LEP_MOD_RAD) {
-        command[2] |= 0x40;
+        mod |= 0x40;
     }
+    
+    tx.address = regRE(4);
+    tx.data = ((0x00FF & mod) << 8) + com + 0x01;
+    write(device, &tx, sizeof(write_register));     
+
+    //command[1] = 0x08;
+    //memcpy(&command[2], data, (length * 2));
+    
+    //write(device, command, (length * 2) + 2);       
+
+    /* set the data length register (big endian) */
+    //command[1] = 0x06; /* data length register */
+    //command[2] = 0x00;
+    //command[3] = length;
+    //write(device, command, 4);    
+    
+    /* set module id, command id, and command type */
+    //command[1] = 0x04;  /* command register */
+    //command[2] = 0x0F & mod;
+    //command[3] = com + 0x01;
 
     /* write to command register */
-    write(device, command, 4);
+    //write(device, command, 4);
     
     if(waitForReady(10)) {
         return -1;
@@ -213,9 +263,6 @@ int writeData(uint8_t mod, uint8_t com, uint8_t * data, uint8_t length) {
 int main(int argc, char *argv[])
 {
     int result;
- 
-    tx_data[0] = 0x00;
-    tx_data[1] = 0x04;
 
     printf("opening device\n");
     
@@ -234,39 +281,36 @@ int main(int argc, char *argv[])
     
     printf("successfully opened port %d\n", result);
     
-    tx_data[0] = 0x00;
-    tx_data[1] = 0x00;
-    tx_data[2] = 0x00;
-    tx_data[3] = 0x01;    
+    if(readData(LEP_MOD_AGC, 0x00, 2) > 0) {
+        printf("AGC enabled: %d\n", enumRE(rx_data));
+    }
+    
+    tx_data[0] = enumRE(1);
     
     writeData(LEP_MOD_AGC, 0x00, tx_data, 2);
     printf("Set AGC Enable: %d\n", status_code);
 
     if(readData(LEP_MOD_AGC, 0x00, 2) > 0) {
-        printf("AGC enabled: %d\n", rx_data[3]);
+        printf("AGC enabled: %d\n", enumRE(rx_data));
     }
     
     if(readData(LEP_MOD_AGC, 0x08, 4) > 0) {
-        printf("AGC ROI: startcol: %d startrow: %d endcol: %d endrow: %d\n", rx_data[1], rx_data[3], rx_data[5], rx_data[7]);
+        printf("AGC ROI: startcol: %d startrow: %d endcol: %d endrow: %d\n", regRE(rx_data), regRE(rx_data[1]), regRE(rx_data[2]), regRE(rx_data[3]));
     }
     
     if(readData(LEP_MOD_AGC, 0x0C, 4) > 0) {
-        printf("AGC HIST: min: %d max: %d mean: %d num: %d\n", rx_data[3], rx_data[5], rx_data[7], rx_data[9]);
+        printf("AGC HIST: min: %d max: %d mean: %d num: %d\n", regRE(rx_data), regRE(rx_data[1]), regRE(rx_data[2]), regRE(rx_data[3]));
     }
     
     if(readData(LEP_MOD_AGC, 0x24, 1) > 0) {
-        printf("AGC DAMPENING: %d\n", rx_data[1]);
+        printf("AGC DAMPENING: %d\n", regRE(rx_data));
     }
     
     /* shutter position */
     if(readData(LEP_MOD_SYS, 0x38, 2) > 0) {
-        printf("Shutter position: %d\n", rx_data[3]);
+        printf("Shutter position: %d\n", enumRE(rx_data));
     }
     
-    tx_data[0] = 0x00;
-    tx_data[1] = 0x00;
-    tx_data[2] = 0x00;
-    tx_data[3] = 0x01;    
     
     //printf("close shutter: %d\n", writeData(LEP_MOD_SYS, 0x38, tx_data, 2));
     
